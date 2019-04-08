ef17a2da536b6b3e4886e765addee4eae7f11d09
DavidGomezSancho <a-olricardo@hotmail.com>
2019-04-08 12:40:37 +0200

    Estructura base de la parte de Multa

--- ejercicio06multas/src/main/java/com/curso/java/spring/boot/Ejercicio06multasDataAplication.java
+++ ejercicio06multas/src/main/java/com/curso/java/spring/boot/Ejercicio06multasDataAplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ejemplo14springBootRestJpaDataApplication {
-
+public class Ejercicio06multasDataAplication {
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Ejemplo14springBootRestJpaDataApplication.class, args);
+		SpringApplication.run(Ejercicio06multasDataAplication.class, args);
 	}
 
 }
--- ejercicio06multas/src/main/java/com/curso/java/spring/boot/model/entities/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.curso.java.spring.boot.model.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository  extends JpaRepository<Cliente, Integer>{
-
-}
