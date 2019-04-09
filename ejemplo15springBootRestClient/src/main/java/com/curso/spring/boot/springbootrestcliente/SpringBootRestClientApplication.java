af673847bea65b0ab9390c27a00abd95ec6ab4f4
CMRG <39797586+CMRG@users.noreply.github.com>
2019-04-09 09:24:03 +0200

    Clientes servicios Rest Spring Boot

--- ejemplo15springBootRestClient/src/main/java/com/curso/spring/boot/springbootrestcliente/SpringBootRestClientApplication.java
+++ ejemplo15springBootRestClient/src/main/java/com/curso/spring/boot/springbootrestcliente/SpringBootRestClientApplication.java
@@ -2,7 +2,6 @@ package com.curso.spring.boot.springbootrestcliente;
 
 import java.util.Arrays;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,32 +10,27 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.curso.java.cliente.spring.boot.ClienteApplication;
 import com.curso.java.cliente.spring.boot.model.entities.Cliente;
 
 //@SpringBootApplication
 public class SpringBootRestClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClienteApplication.class, args);
-		
+//		SpringApplication.run(ClienteApplication.class, args);
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<Cliente[]> httpEntity = new HttpEntity<Cliente[]>(headers);
 		RestTemplate template = new RestTemplate();
-		ResponseEntity<Cliente[]> response = 
-				template.exchange(
-						"http://localhost:8080/clientes", 
-						HttpMethod.GET,
-						httpEntity,
-						Cliente[].class);
+		ResponseEntity<Cliente[]> response = template.exchange("http://localhost:8080/clientes", HttpMethod.GET,
+				httpEntity, Cliente[].class);
 		HttpStatus statusCode = response.getStatusCode();
-		
-		if(statusCode == HttpStatus.OK) {
+
+		if (statusCode == HttpStatus.OK) {
 			Cliente[] clientes = response.getBody();
-			if(clientes != null) {
-				for(Cliente cliente : clientes) {
+			if (clientes != null) {
+				for (Cliente cliente : clientes) {
 					System.out.println(cliente.getNombre());
 				}
 			}
